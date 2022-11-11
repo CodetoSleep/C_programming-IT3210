32c5c2feb91dcc56c936ec69c08b615085b86780
Bùi Phương Nam <93051351+nambpsoict@users.noreply.github.com>
2022-11-11 07:53:47 +0700

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void inbang(int add){
+    int mul = 1;
+	for (int i = 1; i <= 9; i++){
+		for (int j = add; j <= add + 2; j++){
+			printf("| %d x %d = %-2d ", j, mul, j * mul);
+		}
+		printf("|\n");
+		mul++;	
+		if (i % 10 == 0){
+			printf("========================================\n");
+			mul = 1;
+			add += 3;
+		}
+	}
+    printf("----------------------------------------\n");
+}
+int main(){
+    printf("-------------BANG CUU CHUONG-------------\n");
+	printf("========================================\n");
+	int add = 1;
+	int mul = 1;
+	inbang(1);
+    inbang(4);
+    inbang(7);
+    printf("========================================\n");
+	return 0;
+}
